2f7a469cd75e97287d3f07fd1208c8571e69d25f
taylanunutmaz <taylanunutmaz@gmail.com>
2019-02-25 14:31:24 +0300

    nokta koydum

--- AlgoritmaQuiz1.c
+++ AlgoritmaQuiz1.c
@@ -1,65 +1,80 @@
-//tek bir dizi ile iki ayrı stack tut
+//tek bir dizi ile iki ayrı stack tut.
 #define kapasite 50
 #include <stdio.h>
 
 char stack[kapasite] = {' '};
-short top = 0, top2 = kapasite-1;
+short top = 0, top2 = kapasite - 1;
 
-void push(short nereye, char neyi){
-    if(top == top2){
+void push(short nereye, char neyi)
+{
+    if (top == top2)
+    {
         printf("Stack overflow!\n");
     }
-    else{
-        if(nereye == 0) stack[top++] = neyi;
-        else stack[top2--] = neyi;
+    else
+    {
+        if (nereye == 0)
+            stack[top++] = neyi;
+        else
+            stack[top2--] = neyi;
     }
 }
 
-void pop(short neresi){
-    if(neresi==0){
-        if(top == 0){
+void pop(short neresi)
+{
+    if (neresi == 0)
+    {
+        if (top == 0)
+        {
             printf("There is nothing to pop it!\n");
         }
-        else{
+        else
+        {
             top--;
             printf("Stack1 is popped\n");
         }
     }
-    else {
-        if(top2 == kapasite-1){
+    else
+    {
+        if (top2 == kapasite - 1)
+        {
             printf("There is nothing to pop it!\n");
         }
-        else{
+        else
+        {
             top2++;
             printf("Stack2 is popped\n");
         }
     }
 }
 
-
-void display(short neresi){
+void display(short neresi)
+{
     short i;
-    if(neresi == 0){
+    if (neresi == 0)
+    {
         printf("Stack 1:\n");
-        for(i = top-1; i >=0; i--) printf("%c", stack[i]);
+        for (i = top - 1; i >= 0; i--)
+            printf("%c", stack[i]);
         printf("\n");
     }
-    else{
+    else
+    {
         printf("Stack 2:\n");
-        for(i = top2+1; i < kapasite ; i++) printf("%c", stack[i]);
+        for (i = top2 + 1; i < kapasite; i++)
+            printf("%c", stack[i]);
         printf("\n");
     }
 }
 
-
-int main(){
+int main()
+{
 
     push(0, 'a');
     push(0, 'b');
     push(1, 'c');
     push(1, 'd');
     push(0, 'e');
-
 
     display(0);
     display(1);
